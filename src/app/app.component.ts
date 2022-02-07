8d1f51eb6aac791798e5ef13544923a938cfa2a2
Daniel Rojo <danielrojo@gmail.com>
2022-02-07 17:47:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'samples';
+
+  display = 'samples';
+
+  handleClick(value: number) {
+    this.display = '' + value;
+  }  
 }
