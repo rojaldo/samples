a6540b86c427e062a003edd2e05f80241ef4f1f6
Daniel Rojo <danielrojo@gmail.com>
2022-02-17 15:42:57 +0100

    responses implemented in trivial

--- src/app/components/trivial/card/card.component.ts
+++ src/app/components/trivial/card/card.component.ts
@@ -9,10 +9,40 @@ import { Card } from 'src/app/model/card';
 export class CardComponent implements OnInit {
 
   @Input() card!: Card;
-  
+
+  classes: string[] = [
+    'btn btn-primary btn-lg btn-block',   
+    'btn btn-primary btn-lg btn-block', 
+    'btn btn-primary btn-lg btn-block', 
+    'btn btn-primary btn-lg btn-block'];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleClick(answer: string) {
+    this.card.responded = true;
+    this.card.sendResponse(answer);
+    for (let i = 0; i < this.classes.length; i++) {
+      this.classes[i] = this.getClass(this.card.answers[i]);
+    }
+  }
+
+  getClass(answer: string) {
+    console.log('getClass()');
+    
+    if (!this.card.responded) {
+      return 'btn btn-primary btn-lg btn-block';
+    }
+    else {
+      if (this.card.userAnswer === answer && !this.card.rightResponded) {
+        return 'btn btn-danger btn-lg btn-block';
+      } else if (this.card.rightAnswer === answer) {
+        return 'btn btn-success btn-lg btn-block';
+      }
+      return 'btn btn-secondary btn-lg btn-block';
+    }
   }
 
 }
--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -2,11 +2,19 @@ export class Card{
     question = '';
     rightAnswer = '';
     answers: string[] = [];
+    responded = false;
+    rightResponded!: boolean;
+    userAnswer!: string;
     
     constructor(json: any){
         this.question = json.question;
         this.rightAnswer = json.correct_answer;
         this.answers = json.incorrect_answers;
         this.answers.push(this.rightAnswer);
+    }
+
+    sendResponse(answer: string){
+        this.rightResponded = this.rightAnswer === answer;
+        this.userAnswer = answer;
     }
 }
