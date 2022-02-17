da15727c6e3a3012b1e60260c946ed99a3b53660
Daniel Rojo <danielrojo@gmail.com>
2022-02-17 17:21:59 +0100

    shuffle answers and decoding

--- src/app/model/card.ts
+++ src/app/model/card.ts
@@ -11,7 +11,16 @@ export class Card{
         this.rightAnswer = json.correct_answer;
         this.answers = json.incorrect_answers;
         this.answers.push(this.rightAnswer);
+        this.shuffle();
     }
+
+    shuffle(){
+        for (let i = this.answers.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.answers[i], this.answers[j]] = [this.answers[j], this.answers[i]];
+        }
+    }
+    
 
     sendResponse(answer: string){
         this.rightResponded = this.rightAnswer === answer;
